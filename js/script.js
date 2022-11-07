64cafdf8b50d7ac28e87bed2db51baa5e23aedc6
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-08 01:47:45 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 // Змінні
+let rightPosition = 0;
+let imgBlockPosition = 0;
+let direction = 'right';
+let jump = false;
+let hit = false;
+let timer = null;
+let x = 0;
+let halfWidht = window.screen.width / 2;
+
 let canvas = document.querySelector('#canvas');
 let fsBtn = document.querySelector('#fsBtn');
+let heroImg = document.querySelector('#hero-img');
+let imgBlock = document.querySelector('#img-block');
+let jumpBlock = document.querySelector('#jump-block');
+let hitBlock = document.querySelector('#hit-block');
+
+jumpBlock.style.top = `${window.screen.height / 2 - 144 / 2}px`;
+hitBlock.style.top = `${window.screen.height / 2 - 144 / 2}px`;
+
+heroImg.onclick = e => {
+  e.preventDefult();
+};
+
 fsBtn.onclick = () => {
   if (window.document.fullscreen) {
     fsBtn.src = './images/fullscreen.png';
@@ -10,14 +31,14 @@ fsBtn.onclick = () => {
     canvas.requestFullscreen();
   }
 };
-let heroImg = document.querySelector('#hero-img');
-heroImg.onclick = e => {
-  e.preventDefult();
+
+jumpBlock.onclick = () => {
+  jump = true;
 };
-let imgBlock = document.querySelector('#img-block');
-let rightPosition = 0;
-let imgBlockPosition = 0;
-let direction = 'right';
+
+hitBlock.onclick = () => {
+  hit = true;
+};
 
 // Функції
 const rightHandler = () => {
@@ -69,16 +90,61 @@ const standHandler = () => {
   heroImg.style.top = '0px';
 };
 
-// Оброблювач подій
-let timer = null;
-const lifeCycle = () => {
-  timer = setInterval(() => {
-    standHandler();
-  }, 150);
-};
-let x = 0;
-let halfWidht = window.screen.width / 2;
+const hitHandler = () => {
+  switch (direction) {
+    case 'right': {
+      heroImg.style.transform = 'scale(-1,1)';
+      if (rightPosition > 4) {
+        rightPosition = 0;
+        hit = false;
+      }
+      break;
+    }
+    case 'left': {
+      heroImg.style.transform = 'scale(1,1)';
+      if (rightPosition > 3) {
+        rightPosition = 0;
+        hit = false;
+      }
+      break;
+    }
+    default:
+      break;
+  }
 
+  rightPosition += 1;
+  heroImg.style.left = `-${rightPosition * 288}px`;
+  heroImg.style.top = '-864px';
+};
+
+const jumpHandler = () => {
+  switch (direction) {
+    case 'right': {
+      heroImg.style.transform = 'scale(-1,1)';
+      if (rightPosition > 4) {
+        rightPosition = 0;
+        jump = false;
+      }
+      break;
+    }
+    case 'left': {
+      heroImg.style.transform = 'scale(1,1)';
+      if (rightPosition > 3) {
+        rightPosition = 0;
+        jump = false;
+      }
+      break;
+    }
+    default:
+      break;
+  }
+
+  rightPosition += 1;
+  heroImg.style.left = `-${rightPosition * 288}px`;
+  heroImg.style.top = '-288px';
+};
+
+// Оброблювач подій
 let onTouchStart = e => {
   clearInterval(timer);
 
@@ -109,6 +175,18 @@ window.ontouchstart = onTouchStart;
 
 window.onmouseup = onTouchEnd;
 window.ontouchend = onTouchEnd;
+
+const lifeCycle = () => {
+  timer = setInterval(() => {
+    if (hit) {
+      hitHandler();
+    } else if (jump) {
+      jumpHandler();
+    } else {
+      standHandler();
+    }
+  }, 150);
+};
 
 const start = () => {
   lifeCycle();
